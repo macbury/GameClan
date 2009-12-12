abc66eddc0c14cab8cb789098113817e51df4528
macbury <macbury@gmail.com>
2009-12-12 16:37:20 +0100

    production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,8 +8,6 @@ config.cache_classes = true
 config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 config.action_view.cache_template_loading            = true
-ActionMailer::Base.delivery_method = :smtp  
-ActionMailer::Base.default_charset = 'utf-8'
 # See everything in the log (default is :info)
 # config.log_level = :debug
 
